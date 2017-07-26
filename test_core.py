94853b0604144fe8fb3a8200d3467783c5005498
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-07-26 15:30:07 -0500

    Two function from core will be tested

---
+++ test_core.py
@@ -0,0 +1,14 @@
+import core 
+
+def total_cost():
+    assert core.total_cost(1.00) == 1.07
+    assert core.total_cost(5.00) == 5.07 
+    assert core.total_cost(4.00) == 4.07 
+ 
+
+
+def deposit_refund():
+    assert core.deposit_refund(2.00) == 2.10
+    assert core.deposit_refund(1.00) == 1.10
+    assert core.deposit_refund(3.00) == 3.10
+
