ae9896a8375922b40df5489858afe053c23ab4e2
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-07-27 13:11:30 -0500

    I forgot to put the test infront of the functions for them to work

--- test_core.py
+++ test_core.py
@@ -1,13 +1,13 @@
 import core 
 
-def total_cost():
+def test_total_cost():
     assert core.total_cost(1.00) == 1.07
     assert core.total_cost(5.00) == 5.07 
     assert core.total_cost(4.00) == 4.07 
  
 
 
-def deposit_refund():
+def test_deposit_refund():
     assert core.deposit_refund(2.00) == 2.10
     assert core.deposit_refund(1.00) == 1.10
     assert core.deposit_refund(3.00) == 3.10
