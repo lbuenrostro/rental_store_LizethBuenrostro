2405f953da6cc3b0f5907586583dcf1f852dd36b
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-07-27 10:08:33 -0500

    Made inventory items show to the user that way they can see what items are in stock for rent

--- shell.py
+++ shell.py
@@ -1,4 +1,5 @@
-# import core
+import core
+import disk
 import time 
 import sys
 typing_speed = 25
@@ -7,13 +8,18 @@ def slow_type(t):
         sys.stdout.write(l) 
         sys.stdout.flush() 
         time.sleep(typing_speed / 970.0)
-    return input
+    return input()
 
 def main():
-    slow_type('\nWELCOME to Tennis Equipment Rental\n')
-    slow_type('\nEntering...\n')
-    time.sleep(1)
-    
-    print('Thanks for stopping by, come back SOON!!') 
+    slow_type('\n🏸WELCOME to Tennis Rental Agency🏸.Press Enter... \n')
+    item = slow_type("what would you like today?🏸\n")
+    inventory = disk.load_inventory()
+    print(inventory)
+    item = slow_type("What type of equipment would you like?\t").title().strip()
+    price = core.price_of(inventory, item) 
+    print(price)    
+
+    # update_inventory(item, price, items_left)      
+    print('🏸Thanks for stopping by, come back SOON!!🏸') 
 if __name__ == '__main__':
     main()
