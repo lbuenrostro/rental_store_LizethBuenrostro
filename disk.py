7f533854274d4a3966fccc086cb42c1071b20460
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-07-27 09:46:49 -0500

    I moved function_total to disk where I can do a with open statement

--- disk.py
+++ disk.py
@@ -1,6 +1,20 @@
-def update_inventory(item, price, items_left):
+def update_history(item, price, items_left):
     ''' str, float, float _> None
     '''
     msg = item + ', ' + str(price) + ', ' + str(items_left) + '\n'
     with open('history.txt', 'a') as file:
         file.write(msg)
+
+def load_inventory():
+    with open('inventory.txt', 'r') as file:
+        file.readline()
+        items = file.readlines()
+    inventory = []
+    for element in items:
+        pieces = element.split(', ')
+        inventory.append([pieces[0]])
+    return inventory
+
+def function_total(amount, item, days):
+    '''Float, float, float -> float'''
+    
