2c68714b2ba41ffbe4eee20c3ccaa4b90da2709c
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-07-28 09:51:26 -0500

    Added item_inventory function to disk where i can with open

--- disk.py
+++ disk.py
@@ -1,9 +1,9 @@
-def update_history(item, price, items_left):
-    ''' str, float, float _> None
-    '''
-    msg = item + ', ' + str(price) + ', ' + str(items_left) + '\n'
-    with open('history.txt', 'a') as file:
-        file.write(msg)
+# def update_history(item, price, items_left):
+#     ''' str, float, float _> None
+#     '''
+#     msg = item + ', ' + str(price) + ', ' + str(items_left) + '\n'
+#     with open('history.txt', 'a') as file:
+#         file.write(msg)
 
 def load_inventory():
     with open('inventory.txt', 'r') as file:
@@ -43,5 +43,35 @@ def price_of(item_name):
         if item_name in element:
             pieces = element.split(', ')
             price = pieces[1] 
-            return price
+            return float(price)
+        elif item_name not in element:
+            break
     return message
+
+
+    def total_cost(item_price):
+    """ (float) -> float 
+    This function will get the total_cost
+    which will multiply *0.07 for every item
+    >>> total_cost(1.00)
+    1.07
+    >>> total_cost(5.00)
+    5.07 
+    >>> total_cost(4.00)
+    4.07 
+    """
+    message = 'item not found in rental'
+    with open('inventory.txt', 'r') as file:
+        file.readline()
+        items = file.readlines()
+    for item in element:
+        item = item_price * 0.07
+        result = item_price + item
+        return '{:0.2f}'.format(result)
+     elif item_name not in element:
+            break
+    return message
+
+   
+    
+
