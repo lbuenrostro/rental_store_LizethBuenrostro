7c6bb2205d172556fd60c518d3cb5344b5cad2c6
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-07-28 09:52:28 -0500

    Removed item_inventory and moved it to disk

--- core.py
+++ core.py
@@ -1,19 +1,3 @@
-def total_cost(item_price):
-    """ (float) -> float 
-    This function will get the total_cost
-    which will multiply *0.07 for every item
-    >>> total_cost(1.00)
-    1.07
-    >>> total_cost(5.00)
-    5.07 
-    >>> total_cost(4.00)
-    4.07 
-    """
-    item = item_price * 0.07
-    result = item_price + item
-    return '{:0.2f}'.format(result)
-
-
 # def deposit_refund(item_replace):
 #     """ (float) -> float 
 #     This function will get the deposit_refund
