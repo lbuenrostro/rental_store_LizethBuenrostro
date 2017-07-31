98d1e8083b6f1e17c316088888fcd56d8cad75b1
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-07-31 16:16:53 -0500

    deleted deposit function not useful

--- core.py
+++ core.py
@@ -1,20 +1,3 @@
-# def deposit_refund(item_replace):
-#     """ (float) -> float 
-#     This function will get the deposit_refund
-#     which will multiply *0.10 for every item
-#     >>> deposit_refund(2.00)
-#     2.10
-#     >>> deposit_refund(1.00)
-#     1.10
-#     >>> deposit_refund(3.00)
-#     3.10
-#     """
-#     item = item_replace * 0.10
-#     result = item_replace + item
-#     return '{:0.2f}' % result 
-
-
-
 def item_inventory(inventory, item):
     '''str -> str
     return item and price
