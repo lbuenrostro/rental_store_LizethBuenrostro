3815a88584364b70f5759a96d1c879cf1b988909
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-08-09 10:17:56 -0500

    total_deposits from core will be tested

--- test_core.py
+++ test_core.py
@@ -1,17 +1,19 @@
-import core 
+import core
+
 
 def test_tax_cost():
     money = core.tax_cost(10.00)
     expected = '10.70'
     assert money == expected
 
-def test_item_inventory():
-    actual = core.item_inventory([['bob', 25, 4.00]], 'bob')
-    expect = 4.00
-    assert expect
+def test_return_deposit():
+    actual = core.return_deposit(
+        [['bob', 'Pro Tennis Black Bag', 4, 64.20, 7.50]], 'bob')
+    expect = 7.50
+    assert expect == actual
+    actual = core.return_deposit(
+        [['bob', 'Pro Tennis Black Bag', 4, 64.20, 7.50]], 'billy')
+    expect = 'invalid item'
+    assert expect == actual
 
-def test_return_deposit(): 
-    actual = core.return_deposit([['bob', 'Pro Tennis Black Bag', 4, 64.20, 7.50]], 'bob')
-    expect = [['bob', 'Pro Tennis Black Bag', 4, 64.20, 7.50]]
-    assert expect
-
+def total_deposits():
